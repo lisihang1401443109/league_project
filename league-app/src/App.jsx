9cb192fea86f32ac03ab487542838827e75fdfd6
lisihang1401443109 <lisihang1401443109@gmail.com>
2022-08-06 01:30:11 -0500

    me dead

--- league-app/src/App.jsx
+++ league-app/src/App.jsx
@@ -1,26 +1,21 @@
-import React, { useState, } from 'react';
+import React, { useState, useEffect} from 'react';
 import './App.css';
 import axios from 'axios';
-import ReactDOM from 'react-dom/client'
+// import MakeRequest from './makeRequest';
 
 const apiKey = 'RRGAPI-eae9a744-233f-484b-be94-8cf9fd8568ab';
 const apiBase = 'https://na1.api.riotgames.com';
 const apiBaseAmericas = 'https://americas.api.riotgames.com';
 
-class Field extends React.Component{
-
-  render(){
-    return(
-      <div>
-          {this.props.value}
-      </div>
-    )
-  }
-}
-
 function App() {
   const [summonerName, setSummonerName] = useState('');
+  const timeout = 1000
+  const threshold = 20
+  const [reqQueue, setReqQueue] = useState(0)
 
+  useEffect(() => {
+      console.log(reqQueue)
+  }, [reqQueue])
 
   // using the summonerName to get the information including PUUID of the summoner
   function getSummonerInfo(summonerName){
@@ -117,15 +112,41 @@ function App() {
     matchPromise.then(object => object.map(player => getPlayerStats(player)));
   }
 
+  async function tempHandler(e){
+    e.preventDefault()
+ 
+    for (let i = 0; i < 100; i++){
+      await makeRequest('https://www.google.com')
+    }
+  }
+
+  function makeRequest(req){
+    if (reqQueue >= threshold){
+        console.log('above threshold')
+        setTimeout(()=>{makeRequest(req)}, timeout)
+    }else{
+        console.log('requesting, count = ' + reqQueue)
+        // console.log('adding')
+        setReqQueue(val => val+1)
+        return axios.get(req).then(response => {
+            setTimeout(() => {
+                console.log('substracting')
+                setReqQueue(val => val-1)
+            }, timeout);
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+}
+
   return (
     <div className="App">
-      <input type="text" className="summoner-name-input" placeholder="summoner-name" onChange={e => setSummonerName(e.target.value)}></input>
-      <button className="search-btn" onClick={e => searchHandler(e, summonerName)}>Search</button>
+      {/* <input type="text" className="summoner-name-input" placeholder="summoner-name" onChange={e => setSummonerName(e.target.value)}></input> */}
+      {/* <button className="search-btn" onClick={e => searchHandler(e, summonerName)}>Search</button> */}
+      <button className='search-btn' onClick={e => tempHandler(e)}>test</button>
       <div id="container"></div>
     </div>
   );
 }
-
-// const root = ReactDOM.createRoot(document.getElementById("container"));
 
 export default App;
